8af2dc649d7a8804a44ebbaa4d4886e0e87444ec
Chris Rodgers <devdigression@gmail.com>
2018-11-02 20:30:59 -0400

    routers setup

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
 
 // DB config
 const db = require("./config/keys").mongoURI;
@@ -14,6 +19,11 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Welcome to DevHive!");
 });
+
+// Use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
