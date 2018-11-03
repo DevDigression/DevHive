41fcd7967c1cd54cc32c2bec1f5734507cd8b063
Chris Rodgers <devdigression@gmail.com>
2018-11-03 09:14:15 -0400

    bcrypt setup

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 const User = require("../../models/User");
 
@@ -34,6 +35,19 @@ router.post("/register", (req, res) => {
         email: req.body.email,
         avatar: avatar,
         password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            throw err;
+          }
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
