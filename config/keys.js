07d25de456fbd5fc644709e24f0e68084e07c4a3
Chris Rodgers <devdigression@gmail.com>
2018-11-03 09:34:56 -0400

    added JWT retrieval on login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://devhive:devhive123@ds151293.mlab.com:51293/devhive"
+  mongoURI: "mongodb://devhive:devhive123@ds151293.mlab.com:51293/devhive",
+  secret: "secret123"
 };
