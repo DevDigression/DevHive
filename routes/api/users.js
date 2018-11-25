008045bb7a6a83263b0ec7cef78cc1574593d0d3
Chris Rodgers <devdigression@gmail.com>
2018-11-25 15:33:18 -0500

    implemented name validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+
+// Load User model
 const User = require("../../models/User");
 
 // @route   GET /api/users/test
@@ -19,6 +23,13 @@ router.get("/test", (req, res) => {
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({
