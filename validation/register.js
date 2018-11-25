36d5ca5a28655706ca440ce2f91526707415ef22
Chris Rodgers <devdigression@gmail.com>
2018-11-25 16:07:03 -0500

    validation added to all login form fields

--- validation/register.js
+++ validation/register.js
@@ -18,6 +18,10 @@ module.exports = function validateRegisterInput(data) {
     errors.name = "Name field is required";
   }
 
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
