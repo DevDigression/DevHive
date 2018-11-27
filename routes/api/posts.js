03798c74906a3018c44509bdc9f45407949c706b
Chris Rodgers <devdigression@gmail.com>
2018-11-26 20:33:28 -0500

    DELETE comment functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -162,6 +162,14 @@ router.post(
   "/comment/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
     Post.findById(req.params.id)
       .then(post => {
         const newComment = {
@@ -173,6 +181,41 @@ router.post(
 
         // Add to comments array
         post.comments.unshift(newComment);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "Post not found" }));
+  }
+);
+
+// @route   DELETE /api/posts/comment/:id/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentdoesnotexist: "Comment does not exist" });
+        }
+
+        // Get index of comment to remove
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        // Splice comment out of array
+        post.comments.splice(removeIndex, 1);
 
         // Save
         post.save().then(post => res.json(post));
