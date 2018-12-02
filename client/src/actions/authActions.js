6ca9582a4981982e2da76b0510082c29b5407b2e
Chris Rodgers <devdigression@gmail.com>
2018-12-02 18:52:07 -0500

    added function to set auth token to local storage

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,10 +1,35 @@
 import { GET_ERRORS } from "./types";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
 
 export const registeruser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
     .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+  return {
+    type: GET_ERRORS,
+    payload: userData
+  };
+};
+
+// Login - get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Get user token
+      const { token } = res.data;
+      // Set token to local storage
+      localStorage.setItem("jwtToken", token);
+      // Set token to auth header
+      setAuthToken(token);
+    })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
